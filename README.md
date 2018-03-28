509c489be3e91169d078910605c9999686719b82
Golo Roden <golo.roden@thenativeweb.io>
2018-03-28 06:53:41 +0200

    Make performance tests run.

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ $ bot test-performance
 
 ## License
 
-Copyright (c) 2016-2017 the native web.
+Copyright (c) 2016-2018 the native web.
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
--- test/performance/repository/RepositoryTests.js
+++ test/performance/repository/RepositoryTests.js
@@ -3,7 +3,6 @@
 const path = require('path');
 
 const assert = require('assertthat'),
-      cases = require('cases'),
       EventStore = require('sparbuch/lib/postgres/Sparbuch'),
       measureTime = require('measure-time'),
       runfork = require('runfork'),
@@ -16,7 +15,7 @@ const buildEvent = require('../../helpers/buildEvent'),
       FlowAggregate = require('../../../repository/FlowAggregate'),
       Repository = require('../../../repository/Repository');
 
-const app = tailwind.createApp();
+const app = tailwind.createApp({});
 
 const { flows } = new WolkenkitApplication(path.join(__dirname, '..', '..', '..', 'app'));
 
@@ -31,37 +30,35 @@ suite('Repository', function () {
 
   const eventStore = new EventStore();
 
-  suiteSetup(done => {
-    eventStore.initialize({
+  suiteSetup(async () => {
+    await eventStore.initialize({
       url: env.POSTGRES_URL_PERFORMANCE,
       namespace: 'testflows'
-    }, done);
+    });
   });
 
-  suiteTeardown(() => {
-    // We don't explicitly run eventStore.destroy() here, because it caused
-    // strange problems on CircleCI. The tests hang in the teardown function.
-    // This can be tracked down to disposing and destroying the internal pool
-    // of knex, which is provided by pool2. We don't have an idea WHY it works
-    // this way, but apparently it does.
+  suiteTeardown(async () => {
+    await eventStore.destroy();
   });
 
-  setup(done => {
-    runfork({
-      path: path.join(__dirname, '..', '..', 'helpers', 'runResetPostgres.js'),
-      env: {
-        NAMESPACE: 'testflows',
-        URL: env.POSTGRES_URL_PERFORMANCE
-      },
-      onExit (exitCode) {
-        if (exitCode > 0) {
-          return done(new Error('Failed to reset PostgreSQL.'));
-        }
-        done(null);
-      }
-    }, err => {
-      if (err) {
-        return done(err);
+  setup(async () => {
+    await new Promise(async (resolve, reject) => {
+      try {
+        runfork({
+          path: path.join(__dirname, '..', '..', 'helpers', 'runResetPostgres.js'),
+          env: {
+            NAMESPACE: `testflows`,
+            URL: env.POSTGRES_URL_PERFORMANCE
+          },
+          onExit (exitCode) {
+            if (exitCode > 0) {
+              return reject(new Error('Failed to reset PostgreSQL.'));
+            }
+            resolve();
+          }
+        });
+      } catch (ex) {
+        reject(ex);
       }
     });
   });
@@ -72,7 +69,7 @@ suite('Repository', function () {
         flowId,
         repository;
 
-    setup(done => {
+    setup(() => {
       domainEvent = buildEvent('performanceTests', 'stateful', 'first', {});
       flowId = uuid();
       flowAggregate = new FlowAggregate({
@@ -84,13 +81,13 @@ suite('Repository', function () {
 
       repository = new Repository();
 
-      repository.initialize({ app, flows, eventStore }, done);
+      repository.initialize({ app, flows, eventStore });
     });
 
     suite('saveAggregate', () => {
-      test('writes events.', cases([
-        [ 10000, 1 * 1000 ]
-      ], (count, expectedMilliseconds, done) => {
+      test('writes events.', async () => {
+        const count = 10000;
+
         for (let i = 0; i < count; i++) {
           flowAggregate.instance.events.publish('transitioned', {
             state: { is: 'transitioning' }
@@ -99,49 +96,41 @@ suite('Repository', function () {
 
         const getElapsed = measureTime();
 
-        repository.saveAggregate(flowAggregate, err => {
-          const elapsed = getElapsed();
+        await repository.saveAggregate({ aggregate: flowAggregate });
 
-          outputElapsed(elapsed);
+        const elapsed = getElapsed();
 
-          assert.that(err).is.null();
-          assert.that(elapsed.millisecondsTotal).is.lessThan(expectedMilliseconds);
-          done();
-        });
-      }));
+        outputElapsed(elapsed);
+        assert.that(elapsed.millisecondsTotal).is.lessThan(15 * 1000);
+      });
     });
 
     suite('loadAggregateForDomainEvent', () => {
-      test('loads a aggregate.', cases([
-        [ 1000, 0.01 * 1000 ]
-      ], (count, expectedMilliseconds, done) => {
+      test('loads a aggregate.', async () => {
+        const count = 1000;
+
         for (let i = 0; i < count; i++) {
           flowAggregate.instance.events.publish('transitioned', {
             state: { is: 'transitioning' }
           });
         }
 
-        repository.saveAggregate(flowAggregate, errSaveAggregate => {
-          assert.that(errSaveAggregate).is.null();
+        await repository.saveAggregate({ aggregate: flowAggregate });
 
-          const getElapsed = measureTime();
+        const getElapsed = measureTime();
 
-          repository.loadAggregateForDomainEvent({
-            aggregate: { name: 'performanceTestsStateful', id: flowId },
-            domainEvent
-          }, (err, aggregate) => {
-            const elapsed = getElapsed();
-
-            outputElapsed(elapsed);
-
-            assert.that(err).is.null();
-            assert.that(aggregate.instance.revision).is.equalTo(count);
-            assert.that(aggregate.api.forTransitions.state).is.equalTo({ is: 'transitioning' });
-            assert.that(elapsed.millisecondsTotal).is.lessThan(expectedMilliseconds);
-            done();
-          });
+        const aggregate = await repository.loadAggregateForDomainEvent({
+          aggregate: { name: 'performanceTestsStateful', id: flowId },
+          domainEvent
         });
-      }));
+
+        const elapsed = getElapsed();
+
+        outputElapsed(elapsed);
+        assert.that(aggregate.instance.revision).is.equalTo(count);
+        assert.that(aggregate.api.forTransitions.state).is.equalTo({ is: 'transitioning' });
+        assert.that(elapsed.millisecondsTotal).is.lessThan(0.01 * 1000);
+      });
     });
   });
 });
